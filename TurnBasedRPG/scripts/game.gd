212877bacad5ffb6bb32af890d850dd25b3426d0
abdunzelil <93703261+abdunzelil@users.noreply.github.com>
2024-06-26 13:05:00 +0300

    Map and Level mechanics added

--- TurnBasedRPG/scripts/game.gd
+++ TurnBasedRPG/scripts/game.gd
@@ -1,10 +1,31 @@
 extends Node2D
 
 var turn = 0
+var boss_damage = 0
+var boss_heal = 0
+var boss = null
+var boss_path
+var level
+	
 # 0, 2, 4... çift sayı ise -> Boss'un turu
 # 1, 3, 5... tek sayı ise -> Bizim turumuz
 
 func _ready(): # --> Oyun başlangıcında ayarlanacak şeyler
+	$PauseScreen.visible = false
+	$Boss/Spider.visible = false
+	$Boss/VampireBoss.visible = false
+	if Boss.which_boss == 1:
+		level = 1
+		boss_path = $Boss/Spider
+		boss_path.visible = true
+		boss_damage = 8
+		boss_heal = 0
+	elif Boss.which_boss == 2:
+		level = 2
+		boss_path = $Boss/VampireBoss
+		boss_path.visible = true
+		boss_damage = 10
+		boss_heal = 3
 	if CharacterSelection.choice == 1:
 		$Player/Sprite2D.texture = preload("res://assets/elementalist.png")
 	elif CharacterSelection.choice == 2:
@@ -23,8 +44,9 @@ func play_turn():
 		show_turn_label_text("Boss' Turn")
 		$Timer.start(1)
 		await $Timer.timeout
-		$Boss/AnimationPlayer.play("deal_damage")
-		$Player/PlayerHPBar.value -= 8
+		boss_path.get_node("AnimationPlayer").play("deal_damage")
+		$Player/AnimationPlayer.play("take_damage")
+		boss_does_something(boss_damage, boss_heal)
 		if $Player/PlayerHPBar.value <= 0:
 			$ColorRect.visible = true
 			return
@@ -37,6 +59,11 @@ func play_turn():
 		toggle_buttons()
 		pass
 		
+		
+func boss_does_something(damage: int, heal: int):
+	$Player/PlayerHPBar.value -= damage
+	boss_path.get_node("BossHPBar").value += heal
+	
 func toggle_buttons():
 	if $Button.disabled == false: #Basabiliyorsam hepsini disabled yap
 		$Button.disabled = true
@@ -58,8 +85,14 @@ func show_turn_label_text(text: String):
 
 func _on_button_pressed(): # Slash butonuna basıldığında
 	$Player/AnimationPlayer.play("deal_damage")
-	$Boss/AnimationPlayer.play("take_damage")
-	$Boss/BossHPBar.value -= 5
+	boss_path.get_node("AnimationPlayer").play("take_damage")
+	boss_path.get_node("BossHPBar").value -= 5
+	if boss_path.get_node("BossHPBar").value <= 0:
+		if level in CompletedLevels.completed_levels: # Daha önce kestik
+			pass
+		else: # İlk defa kesiyorsak
+			CompletedLevels.completed_levels.append(level)
+		get_tree().change_scene_to_file("res://scenes/map.tscn")
 	turn += 1
 	toggle_buttons()
 	play_turn()
@@ -69,9 +102,15 @@ func _on_button_2_pressed(): # Fireball butonuna basıldığında
 		pass
 	else: # Yeterli manamız varsa
 		$Player/AnimationPlayer.play("deal_damage")
-		$Boss/AnimationPlayer.play("take_damage")
-		$Boss/BossHPBar.value -= 15
+		boss_path.get_node("AnimationPlayer").play("take_damage")
+		boss_path.get_node("BossHPBar").value -= 15
 		$Player/PlayerManaBar.value -= 15
+		if boss_path.get_node("BossHPBar").value <= 0:
+			if level in CompletedLevels.completed_levels: # Daha önce kestik
+				pass
+			else: # İlk defa kesiyorsak
+				CompletedLevels.completed_levels.append(level)
+			get_tree().change_scene_to_file("res://scenes/map.tscn")
 		turn += 1
 		toggle_buttons()
 		play_turn()
@@ -88,10 +127,20 @@ func _on_button_4_pressed():
 	toggle_buttons()
 	play_turn()
 
-
 func _on_restart_pressed():
 	get_tree().reload_current_scene()
 
-
 func _on_back_to_mm_pressed():
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+
+func _on_pause_button_pressed():
+	$PauseScreen.visible = true
+	get_tree().paused = true
+
+func _on_resume_pressed():
+	$PauseScreen.visible = false
+	get_tree().paused = false
+
+func _on_main_menu_pressed():
+	get_tree().paused = false
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
